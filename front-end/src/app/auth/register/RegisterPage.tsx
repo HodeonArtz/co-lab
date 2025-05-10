dbf318a1d290489df815fc470077e856402334e8
HodeonArtz <martidicianolold@gmail.com>
2025-05-10 18:22:55 +0200

    feat #28: create register page

--- front-end/src/app/auth/register/RegisterPage.tsx
+++ front-end/src/app/auth/register/RegisterPage.tsx
@@ -1,5 +1,61 @@
+import {
+  Anchor,
+  Box,
+  Button,
+  PasswordInput,
+  Stack,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import { useForm, zodResolver } from "@mantine/form";
+import { Link } from "react-router-dom";
+import { authSchema } from "../../../validations/zodAuthSchema";
+
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const registerForm = useForm<AuthForm>({
+    mode: "controlled",
+    validate: zodResolver(authSchema),
+    validateInputOnChange: true,
+  });
+  return (
+    <Stack align="stretch">
+      <Title ta="center" size="1.85rem" mb="lg">
+        Sign up and start co.labing!
+      </Title>
+      <Title ta="center" order={2}>
+        Register
+      </Title>
+      <Box component="form" onSubmit={registerForm.onSubmit(() => {})}>
+        <TextInput
+          label="Username"
+          placeholder="New username"
+          size="md"
+          radius="md"
+          mb="xs"
+          {...registerForm.getInputProps("username")}
+        />
+        <PasswordInput
+          label="Password"
+          placeholder="New password"
+          size="md"
+          radius="md"
+          mb="xl"
+          {...registerForm.getInputProps("password")}
+        />
+        <Button type="submit" fullWidth mt="xl" size="md" radius="md">
+          Register
+        </Button>
+
+        <Text ta="center" mt="md">
+          Already have an account?{" "}
+          <Anchor component={Link} to="/auth/login" fw={500}>
+            Log in
+          </Anchor>
+        </Text>
+      </Box>
+    </Stack>
+  );
 };
 
 export default RegisterPage;
