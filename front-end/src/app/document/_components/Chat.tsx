d00e2290517a4daa21c2c0b75f0289da4e142c4d
HodeonArtz <martidicianolold@gmail.com>
2025-05-12 19:13:57 +0200

    feat: add title for chat

--- front-end/src/app/document/_components/Chat.tsx
+++ front-end/src/app/document/_components/Chat.tsx
@@ -1,9 +1,10 @@
-import { Stack } from "@mantine/core";
+import { Stack, Title } from "@mantine/core";
 import MessageEditor, { MessagesChannel } from "./Messages";
 
 const Chat = () => {
   return (
     <Stack justify="end" h="100%">
+      <Title size="h3 ">Chat</Title>
       <MessagesChannel />
       <MessageEditor />
     </Stack>
