0561ed4b1e47a7ecb2a85cae22ce196d6fb6babf
HodeonArtz <martidicianolold@gmail.com>
2025-05-18 20:09:32 +0200

    fix: scrolling

--- front-end/src/app/_components/layout/Navbar.tsx
+++ front-end/src/app/_components/layout/Navbar.tsx
@@ -1,16 +1,52 @@
-import { AppShell, Divider, ScrollArea, Stack, Title } from "@mantine/core";
+import {
+  AppShell,
+  Button,
+  Divider,
+  ScrollArea,
+  Stack,
+  Title,
+} from "@mantine/core";
 import UserDisplay from "../UserDisplay";
-
+import { IconUpload } from "@tabler/icons-react";
+import { Dropzone, FileWithPath } from "@mantine/dropzone";
+import { useRef } from "react";
+import scrollClassnames from "./Scroll.module.css";
 const Navbar = () => {
+  const openRef = useRef<() => void>(null);
+
+  function handleUpload(files: FileWithPath[]) {
+    console.log(files);
+  }
   return (
     <AppShell.Navbar p="md">
       <AppShell.Section>
         <Title size="h3">Files</Title>
       </AppShell.Section>
-      <Divider my="md" />
-      <AppShell.Section grow my="md" component={ScrollArea}>
-        <Stack>f</Stack>
+      <Divider mt="xs" mb="md" />
+
+      <AppShell.Section>
+        <Button
+          w="100%"
+          leftSection={<IconUpload size={18} />}
+          onClick={() => openRef.current?.()}
+          style={{ pointerEvents: "all" }}
+        >
+          Upload file
+        </Button>
+        <ScrollArea></ScrollArea>
       </AppShell.Section>
+      <ScrollArea h="100%" my="md" classNames={scrollClassnames}>
+        <AppShell.Section h="100%">
+          <Dropzone
+            h="100%"
+            openRef={openRef}
+            onDrop={handleUpload}
+            activateOnClick={false}
+          >
+            <Stack></Stack>
+          </Dropzone>
+        </AppShell.Section>
+      </ScrollArea>
       <AppShell.Section>
         <UserDisplay />
       </AppShell.Section>
